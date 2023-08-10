6453e0fc50badf59f90a897f922714fd165fa477
Akanksha Nigam <59891227+akankshanigam@users.noreply.github.com>
2023-08-10 18:08:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,12 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-2" # You can change this to your preferred region
 }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-  owners = ["amazon"] # This owner ID corresponds to Amazon's
-}
-
-resource "aws_instance" "my_instance" {
-  ami           = data.aws_ami.amazon_linux.id
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Example Amazon Linux 2 LTS AMI
   instance_type = "t2.micro"
 
-  key_name    = aws_key_pair.my_key.key_name
-  vpc_security_group_ids = [aws_security_group.my_sg.id]
-
   tags = {
-    Name = "MyInstance"
-  }
-}
-
-resource "aws_key_pair" "my_key" {
-  key_name   = "my-key"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
-
-resource "aws_security_group" "my_sg" {
-  name        = "my_sg"
-  description = "Allow inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    Name = "example-instance"
   }
 }
