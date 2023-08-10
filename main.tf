daffb91d227bf0afa762e805e7ab018e9301f9bb
Akanksha Nigam <59891227+akankshanigam@users.noreply.github.com>
2023-08-10 19:33:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,39 @@
-provider "aws" {
-  region = "eu-west-1"
+provider "google" {
+  credentials = file("${var.credentials_file}")
+  project     = "hadoop"
+  region      = "us-central1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-08a52ddb321b32a8c"
-  instance_type = "t2.micro"
+variable "credentials_file" {
+  description = "Path to the GCP credentials JSON file"
+  type        = string
+}
 
-  tags = {
-    Name = "example-instance"
+resource "google_compute_instance" "default" {
+  name         = "example-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
   }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = "username:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  service_account {
+    scopes = ["compute-ro", "view"]
+  }
+
+  tags = ["web", "prod"]
 }
