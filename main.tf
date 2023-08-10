17ee8b023057e164567683465d31c071ffb5db76
Akanksha Nigam <59891227+akankshanigam@users.noreply.github.com>
2023-08-10 18:15:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
-provider "aws" {
-  region = "us-west-2" # You can change this to your preferred region
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  owners = ["amazon"]
 }
 
-resource "aws_instance" "test" {
-  ami           = "ami-0c55b159cbfafe1f0" # Example Amazon Linux 2 LTS AMI
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
   tags = {
